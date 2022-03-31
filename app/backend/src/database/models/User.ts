d7cfad5f4989c0d16e8d6b1a48a03c7e5219d680
Juan Formoso <trajano.juan@gmail.com>
2022-03-31 15:50:32 -0300

    Update UserModel

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,42 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id: number;
+  public username: string;
+  public email: string;
+  public password: string;
+  public role: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  tableName: 'users',
+  sequelize: db,
+  timestamps: false,
+});
+
+export default User;
