ec1b578571b80829b310317b08d1858e9a35fa07
Juan Formoso <trajano.juan@gmail.com>
2022-04-05 14:43:10 -0300

    Update Rating Interface

---
+++ app/backend/src/interfaces/Rating.ts
@@ -0,0 +1,14 @@
+interface IRating {
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: number;
+}
+
+export default IRating;
