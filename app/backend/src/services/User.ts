69cd0b7879790d96d5c5d94fa82a180e84dc9676
Juan Formoso <trajano.juan@gmail.com>
2022-04-05 19:57:13 -0300

    Update User Service

--- app/backend/src/services/User.ts
+++ app/backend/src/services/User.ts
@@ -2,15 +2,16 @@ import * as fs from 'fs';
 import * as bcryptjs from 'bcryptjs';
 import * as jwt from 'jsonwebtoken';
 import Users from '../database/models/User';
+import verifyJWT from '../middlewares/Auth';
 
-type User = {
-  email: string;
-  password: string;
+type User = { 
+  email: string,
+  password: string,
 };
 
 const getUser = async ({ email, password }: User) => {
   const user = await Users.findOne({ 
-    where: { 
+    where: {
       email,
     } 
   });
@@ -18,8 +19,8 @@ const getUser = async ({ email, password }: User) => {
     return { status: 401, message: 'Incorrect email or password' }; 
   }
   const isMatch = await bcryptjs.compare(password, user.password);
-  if (!isMatch) {
-    return { status: 401, message: 'Incorrect email or password' };
+  if (!isMatch) { 
+    return { status: 401, message: 'Incorrect email or password' }; 
   }
   const secret: string = await fs.readFileSync('jwt.evaluation.key', 'utf8').trim();
   const token = jwt.sign({ email }, secret, { expiresIn: '1h' });
@@ -29,9 +30,21 @@ const getUser = async ({ email, password }: User) => {
       email: user.email,
       username: user.username,
       role: user.role,
-    }
-  }
-  return { status: 200, message: 'Login successful', token, loginResponse };
+    },
+    token,
+  };
+  return { status: 200, message: 'Login sucessfull', token, loginResponse };
 };
 
-export default { getUser };
+const getUserRole = async (token: string) => {
+  const decoded = verifyJWT(token);
+  if (typeof decoded === 'object') {
+    const userData = await Users.findOne({ where: { email: decoded.email } });
+    return userData?.role;
+  }
+};
+
+export {
+  getUser,
+  getUserRole,
+};
