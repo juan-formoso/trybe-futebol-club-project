31d25a0e344e016f08246a34234bf09b3ff3ecae
Juan Formoso <trajano.juan@gmail.com>
2022-04-05 15:12:20 -0300

    Create login validation logic

--- app/backend/src/middlewares/loginValidation.ts
+++ app/backend/src/middlewares/loginValidation.ts
@@ -1,5 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 
-const loginValidation = (req: Request, res: Response, next: NextFunction) => {};
+const loginValidation = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(401).json({
+      message: 'All fields must be filled',
+    });
+  }
+  next();
+};
 
 export default loginValidation;
