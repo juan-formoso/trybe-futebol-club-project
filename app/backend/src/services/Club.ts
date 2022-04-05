dce4cdd18840eb8d6d63a04a421436c7f933a85b
Juan Formoso <trajano.juan@gmail.com>
2022-04-05 19:28:34 -0300

    Update Club Service

---
+++ app/backend/src/services/Club.ts
@@ -0,0 +1,13 @@
+import Club from '../database/models/Club';
+
+const getAll = async () => {
+  const clubs = await Club.findAll();
+  return { status: 200, data: clubs };
+};
+
+const getById = async (id: number) => {
+  const club = await Club.findByPk(id);
+  return { status: 200, data: club };
+};
+
+export default { getAll, getById };
