f03d8b3a94422a2b38ca5579ae494b423ddf7058
Juan Formoso <trajano.juan@gmail.com>
2022-04-06 00:55:15 -0300

    Refatorando token middleware

--- app/backend/src/middlewares/Token.ts
+++ app/backend/src/middlewares/Token.ts
@@ -3,21 +3,16 @@ import * as jwt from 'jsonwebtoken';
 import * as fs from 'fs';
 
 const checkToken = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization;
-  const privateKey = fs.readFileSync('jwt.evaluation.key', 'utf8').trim();
-
+  const token: string | undefined = req.headers.authorization;
+  const secretKey = fs.readFileSync('jwt.evaluation.key', 'utf8').trim();
   if (!token) {
-    return res.status(401).json({
-      message: 'No token provided.'
-    });
+    return res.status(401).json({ message: 'Token not found' });
   }
   try {
-    jwt.verify(token, privateKey);
+    jwt.verify(token, secretKey);
     next();
-  } catch (error) {
-    return res.status(401).json({
-      message: 'Invalid token.'
-    });
+  } catch (e) {
+    return res.status(401).json({ message: 'Invalid token' });
   }
 };
 
