7db9effaf19ecf8c33465efb8a8ec34a21d5579b
Juan Formoso <trajano.juan@gmail.com>
2022-04-06 00:54:31 -0300

    Refatorando Match Controller

--- app/backend/src/interfaces/Match.ts
+++ app/backend/src/interfaces/Match.ts
@@ -1,12 +1,8 @@
 import Match from '../database/models/Match';
 
-interface IMatch {
-  homeClub: {
-    clubName: string;
-  }
-  awayClub: {
-    clubName: string;
-  }
+interface MatchDetail extends Match {
+  homeClub: { clubName: string };
+  awayClub: { clubName: string };
 }
 
-export default IMatch;
+export default MatchDetail;
