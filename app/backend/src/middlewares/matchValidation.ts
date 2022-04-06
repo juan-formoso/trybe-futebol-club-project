55dd30082afc56b137f9e773f5d3a42df2347de4
Avery <trajano.juan@gmail.com>
2022-04-06 03:41:24 -0300

    update middlewares

---
+++ app/backend/src/middlewares/matchValidation.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import ClubModel from '../database/models/Club';
+
+const matchValidate = async (req: Request, res: Response, next: NextFunction) => {
+  const { homeTeam, awayTeam } = req.body;
+  if (homeTeam === awayTeam) {
+    return res.status(401).json({
+      message: 'It is not possible to create a match with two equal teams',
+    });
+  }
+  const homeClubExist = await ClubModel.findByPk(homeTeam);
+  const awayClubExist = await ClubModel.findByPk(awayTeam);
+  if (!homeClubExist || !awayClubExist) {
+    return res.status(401).json({ message: 'There is no team with such id!' });
+  }
+  next();
+};
+
+export default matchValidate;
