d8d0af4ecf4529ae5cbf57d75bf8c147978b2153
Juan Formoso <trajano.juan@gmail.com>
2022-04-06 00:54:38 -0300

    Refatorando Auth middleware

--- app/backend/src/middlewares/Auth.ts
+++ app/backend/src/middlewares/Auth.ts
@@ -1,10 +1,11 @@
-import * as jwt from 'jsonwebtoken';
 import * as fs from 'fs';
+import * as jwt from 'jsonwebtoken';
 
-const verifyJWT = (token: string) => {
-  const privateKey = fs.readFileSync('jwt.evaluation.key', 'utf8').trim();
-  const decoded = jwt.verify(token, privateKey);
+const verifyJWT = (token: string): string | jwt.JwtPayload => {
+  const secretKey = fs.readFileSync('jwt.evaluation.key', 'utf8').trim();
+  const decoded = jwt.verify(token, secretKey);
+
   return decoded;
-}
+};
 
 export default verifyJWT;
