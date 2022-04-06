6de960e24bfb3607ac0efefe6aaac9266a5aace5
Juan Formoso <trajano.juan@gmail.com>
2022-04-06 00:54:51 -0300

    Refatorando login middleware

--- app/backend/src/middlewares/loginValidation.ts
+++ app/backend/src/middlewares/loginValidation.ts
@@ -1,13 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
-const loginValidation = (req: Request, res: Response, next: NextFunction) => {
+const validLogin = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(401).json({
-      message: 'All fields must be filled',
-    });
+    return res.status(401).json({ message: 'All fields must be filled' });
   }
   next();
 };
 
-export default loginValidation;
+export default { validLogin };
