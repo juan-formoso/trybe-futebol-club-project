23bff176b1b199f1af2443f0c906e4cd2a025e4e
Avery <trajano.juan@gmail.com>
2022-04-06 03:55:20 -0300

    Update app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,19 +1,16 @@
 import * as express from 'express';
 import * as cors from 'cors';
-import ClubController from './controllers/Club';
-import MatchController from './controllers/Match';
-import { login, validate } from './controllers/User';
-import validation from './middlewares/loginValidation';
-import checkToken from './middlewares/Token';
+import clubsRoute from './routes/clubs.routes';
+import loginRoute from './routes/login.routes';
+import matchsRoute from './routes/matchs.routes';
+import leaderBordRoute from './routes/leaderboard.routes';
 
 class App {
   public app: express.Express;
-
   constructor() {
     this.app = express();
     this.config();
   }
-
   private config():void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
@@ -21,27 +18,11 @@ class App {
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
-
-    this.app.use(accessControl);
-    this.routes();
-  }
-
-  private routes():void {
     this.app.use(express.json());
     this.app.use(cors());
-    this.app.post('/login', validation.validLogin, login);
-    this.app.get('/login/validate', validate);
-    this.app.get('/clubs', ClubController.getAllClubs);
-    this.app.get('/clubs/:id', ClubController.getClubById);
-    this.app.get('/matchs', MatchController.getMatchs);
-    this.app.post('/matchs', checkToken, MatchController.saveMatch);
-    this.app.patch('/matchs/:id/finish', checkToken, MatchController.finishMatch);
-    this.app.patch('/matchs/:id', checkToken, MatchController.editMatch);
-    this.app.get('/leaderboard', MatchController.getAllRatings);
-    this.app.get('/leaderboard/home', MatchController.getHomeRatings);
-    this.app.get('/leaderboard/away', MatchController.getAwayRatings);
+    this.app.use(accessControl);
+    this.app.use('/', loginRoute, clubsRoute, matchsRoute, leaderBordRoute);
   }
-
   public start(PORT: string | number):void {
     this.app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
   }
