b2d995de3e58f69e8a0d42f77172920a48899116
Juan Formoso <trajano.juan@gmail.com>
2022-04-06 00:54:03 -0300

    Refatorando Club Controller

--- app/backend/src/controllers/Club.ts
+++ app/backend/src/controllers/Club.ts
@@ -1,17 +1,15 @@
 import { Request, Response } from 'express';
-import ClubServices from '../services/Club';
+import ClubService from '../services/Club';
 
-const getAll = async (req: Request, res: Response) => {
-  const clubs = await ClubServices.getAll();
-  res.json(clubs);
+const getAllClubs = async (_req: Request, res: Response) => {
+  const { status, data } = await ClubService.getAll();
+  return res.status(status).json(data);
 };
 
-const getById = async (req: Request, res: Response) => {
-  const club = await ClubServices.getById(req.params.id);
-  res.json(club);
+const getClubById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { status, data } = await ClubService.getById(id);
+  return res.status(status).json(data);
 };
 
-export default {
-  getAll,
-  getById,
-};
+export default { getAllClubs, getClubById };
