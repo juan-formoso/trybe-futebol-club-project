6550f3936fde44544c18e0d931a95a02061ecabd
Juan Formoso <trajano.juan@gmail.com>
2022-04-06 00:54:23 -0300

    Refatorando User Controller

--- app/backend/src/controllers/User.ts
+++ app/backend/src/controllers/User.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from 'express';
-import * as UserServices from '../services/User';
+import * as UserService from '../services/User';
 
 const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-  const { status, message, data } = await UserServices.getUser({ email, password });
+  const { status, message, data } = await UserService.getUser({ email, password });
   if (status >= 400) {
     return res.status(status).json({ message });
   }
   return res.status(200).json(data);
 };
 
-const tokenValidation = async (req: Request, res: Response) => {
+const validate = async (req: Request, res: Response) => {
   const token = req.headers.authorization || '';
-  const data = await UserServices.getUserRole(token);
+  const data = await UserService.getRole(token);
   return res.status(200).json(data);
 };
 
-export default { login, tokenValidation };
+export { login, validate };
